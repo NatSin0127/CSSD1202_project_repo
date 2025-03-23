3af4b205910f14c5d512ba20e27c15d4126c5afc
Natalie <1155174409@link.cuhk.edu.hk>
2025-03-24 07:09:02 +0800

    rename javascript file and syntax fix in script.js

--- Project files/script.js
+++ Project files/script.js
@@ -10,7 +10,7 @@ const enableDarkmode = () => {
 const disableDarkmode = () => {
     document.body.classList.remove('darkmode');
     localStorage.setItem('darkmode', null);
-}
+};
 
 if (darkmode === "active") enableDarkmode();
 
